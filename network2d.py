500c5436467e01e43b9443c1728479c3ca592f45
Doragh Ilyess <ilyess.doragh@student-cs.fr>
2022-12-08 10:49:22 +0100

    2D full Implementation

---
+++ network2d.py
@@ -0,0 +1,206 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+from torch import optim
+from torch.autograd import grad
+from itertools import chain
+import torchsummary
+from real_sol2d import real_sol
+from vrac.bails_sombres import RNN, Transformer
+from variable_speed import c_fun
+from config import DEFAULT_CONFIG
+import numpy as np
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+class Scaling_layer(nn.Module):  # Couche de normalisation des données entre -1 et 1
+    def __init__(self):
+        super(Scaling_layer, self).__init__()
+
+        # On est maintenant en 2D donc on a 3 entrées (x,y,t)
+        self.lb = torch.tensor([0.0, 0.0, 0.0]).to(device)  # lower bound
+        self.ub = torch.tensor([1.0, 1.0, 1.0]).to(device)  # upper bound
+
+    def forward(self, x):
+        return 2 * (x - self.lb) / (self.ub - self.lb)
+
+# Réseau de neurones
+
+
+class network(torch.jit.ScriptModule):
+    def __init__(self, N_neurons, N_layers):
+        super().__init__()
+        self.num_neurons = N_neurons
+        self.num_layers = N_layers
+        self.scaling_layer = Scaling_layer()
+
+        # On est maintenant en 2D donc on a 3 entrées (x,y,t)
+        self.linear_input = nn.Linear(3, self.num_neurons)
+        self.linear_hidden = nn.ModuleList(
+            [nn.Linear(self.num_neurons, self.num_neurons) for _ in range(self.num_layers)])
+        self.linear_output = nn.Linear(self.num_neurons, 1)
+        self.activation = nn.Tanh()
+        self.dropout = nn.Dropout(0.3)
+        self.bn = nn.BatchNorm1d(self.num_neurons)
+
+    def forward(self, x):
+        #x = self.scaling_layer(x)
+        x = self.activation(self.linear_input(x))
+        for i, linear in enumerate(self.linear_hidden):
+            x = self.activation(linear(x))
+            #x = self.bn(x)
+            x = self.dropout(x)
+        x = self.linear_output(x)
+        return x
+
+
+class PINN():
+    def __init__(self, with_rnn=False, N_neurons=64, N_layers=4):
+        if with_rnn == True:
+            #self.net = RNN(2, 64, 1, num_layers=4).to(device)
+
+            # On est maintenant en 2D donc on a 3 entrées (x,y,t)
+            self.net = Transformer(3, 16, 1, num_layers=4).to(device)
+        else:
+            self.net = network(N_neurons, N_layers).to(device)
+        self.optimizer = optim.Adam(
+            self.net.parameters(), lr=DEFAULT_CONFIG['lr'])  # descente de gradient
+        self.scheduler = optim.lr_scheduler.CosineAnnealingLR(
+            self.optimizer, T_max=DEFAULT_CONFIG['epochs'])
+        self.loss_history = []
+        self.loss_history_val = []
+
+    def _model_summary(self):
+        print(torchsummary.summary(self.net, [(32, 1), (32, 1)]))
+
+    # Calculer résidu
+    def nth_gradient(self, f, wrt, n):
+        for i in range(n):
+            f = list(chain(*f))
+            grads = grad(f, wrt, create_graph=True, allow_unused=True,)[0]
+            f = grads
+            if grads is None:
+                print("Bad Grad")
+                return None
+        return grads
+
+    def calculate_laplacian(self, model, tensor):
+        laplacian_x = torch.zeros(tensor.shape[0], 1, device=device)
+        laplacian_t = torch.zeros(tensor.shape[0], 1, device=device)
+
+        # On est maintenant en 2D donc on a 3 entrées (x,y,t)
+        laplacian_y = torch.zeros(tensor.shape[0], 1, device=device)
+
+        for i, tensori in enumerate(tensor):
+            hess = torch.autograd.functional.hessian(
+                model, tensori.unsqueeze(0), create_graph=True)
+            hess = hess.view(2, 2)
+            laplacian_x[i] = hess[0, 0]
+            laplacian_t[i] = hess[1, 1]
+
+            # On est maintenant en 2D donc on a 3 entrées (x,y,t)
+            laplacian_y[i] = hess[2, 2]
+        return laplacian_x, laplacian_y, laplacian_t
+
+    def flat(self, x):
+        m = x.shape[0]
+        return [x[i] for i in range(m)]
+
+    def f(self, x, y, t, variable_speed=False):
+        u_xx, u_yy, u_tt = self.calculate_laplacian(
+            self.net, torch.cat([x, y, t], 1))
+        if variable_speed:
+            c = c_fun(x, y, t)
+            #residual = u_tt - c*u_xx - (c**2-1)*(np.pi**2)*torch.sin(np.pi*x)*torch.sin(np.pi*t)
+
+            # On est maintenant en 2D donc on a 3 entrées (x,y,t)
+            residual = u_tt - c*(u_xx+u_yy)
+        else:
+            residual = u_tt - 4*(u_xx+u_yy)
+            #lap,_ = self.calculate_laplacian(self.net, torch.cat([x, t], 1))
+            #residual = lap - np.pi**2*torch.sin(np.pi*x)*torch.sin(np.pi*t)
+        return residual
+
+    # On est maintenant en 2D donc on a 3 entrées (x,y,t)
+    def loss_first(self, x_ri, y_ri, t_ri):
+        real_solution = real_sol(x_ri, y_ri, t_ri)
+        u_pred_r = self.net(torch.cat([x_ri, y_ri, t_ri], 1))
+        loss_residual = torch.mean((u_pred_r-real_solution)**2)
+        return loss_residual
+
+    # On est maintenant en 2D donc on a 3 entrées (x,y,t)
+    def loss_fn(self, x_r, y_r, t_r,
+                u_b, x_b, y_b, t_b,
+                u_i, x_i, y_i, t_i, validation=False):
+
+        loss_residual = torch.mean(torch.abs(self.f(x_r, y_r, t_r)))
+
+        u_pred_b = self.net(torch.cat([x_b, y_b, t_b], 1))
+        loss_bords = torch.mean((u_pred_b-u_b)**2)
+        u_pred_i = self.net(torch.cat([x_i, y_i, t_i], 1))
+        loss_init = torch.mean((u_pred_i-u_i)**2)
+
+        # Compute derivative of u_pred_b with respect to t_b
+        #u_pred_b_t = torch.autograd.functional.jacobian(self.net, torch.cat([x_b, t_b], 1), create_graph=True)
+        loss_bords_der = torch.zeros(1, device=device)
+        #loss_bords_der = torch.mean((u_pred_b_t)**2)
+
+        """
+        #Add truncated boundary c u_tx - u_tt = 0 at t = 1
+        t_r_1 = torch.ones_like(t_r, requires_grad=True)
+        u_pred_t_1 = self.net(x_r, t_r_1)
+        u_pred_t_1_x = self.nth_gradient(self.flat(u_pred_t_1), wrt=x_r, n=1)
+        u_pred_t_1_x_t = self.nth_gradient(self.flat(u_pred_t_1_x), wrt=t_r_1, n=1)
+        u_tt = self.nth_gradient(self.flat(u_pred_t_1), wrt=t_r_1, n=2)
+        loss_trunc = torch.mean((u_pred_t_1_x_t - u_tt)**2)
+        """
+        loss_trunc = torch.zeros(1, device=device)
+        return loss_residual, loss_bords, loss_init, loss_bords_der, loss_trunc
+
+    def train_step(self, train_data, phase="later"):
+        if phase == "beginning":
+            t_ri, x_ri, y_ri = train_data
+            self.net.train()
+            self.optimizer.zero_grad()
+            loss = self.loss_first(x_ri, y_ri, t_ri)
+            loss.backward()
+            self.optimizer.step()
+            return loss.item()
+        else:
+            x_r, y_r, t_r, u_b, x_b, y_b, t_b, u_i, x_i, y_i, t_i, = train_data
+            self.net.train()
+            self.optimizer.zero_grad()
+            loss_residual, loss_bords, loss_init, loss_bords_der, loss_trunc = self.loss_fn(x_r, y_r, t_r,
+                                                                                            u_b, x_b, y_b, t_b,
+                                                                                            u_i, x_i, y_i, t_i)
+            loss = loss_residual + loss_bords + loss_init + loss_bords_der + 0.5*loss_trunc
+            loss.backward()
+            self.optimizer.step()
+            return loss_residual.item(), loss_bords.item(), loss_init.item(), loss_bords_der.item(), loss_trunc.item()
+
+    def val_step(self, val_data, phase="later"):
+        if phase == "beginning":
+            x_ri, y_ri, t_ri = val_data
+            loss = self.loss_first(x_ri, y_ri, t_ri)
+            return loss.item()
+        else:
+            x_r, y_r, t_r, u_b, x_b, y_b, t_b, u_i, x_i, y_i, t_i = val_data
+            self.net.eval()
+            loss_residual, loss_bords, loss_init, loss_bords_der, loss_trunc = self.loss_fn(x_r, y_r, t_r,
+                                                                                            u_b, x_b, y_b, t_b,
+                                                                                            u_i, x_i, y_i, t_i, validation=False)
+            loss = loss_residual + loss_bords + loss_init + loss_bords_der + loss_trunc
+            return loss.item()
+
+    def accuracy_step(self, val_data):
+        x_r, y_r, t_r, _, _, _, _, _, _ = val_data
+        self.net.eval()
+        # Compute MSE between real_sol and net
+        with torch.no_grad():
+            u_pred = self.net(torch.cat([x_r, y_r, t_r], 1))
+            real_u = real_sol(x_r, y_r, t_r)
+            num = torch.mean(torch.square(u_pred-real_u))
+            #den = torch.mean(torch.square(real_u))
+        return num.item()
---
+++ pinn_training2D.py
@@ -0,0 +1,231 @@
+import torch
+import numpy as np
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+from network2d import PINN
+import torch.functional as F
+from real_sol import real_sol
+from config import DEFAULT_CONFIG
+from dataset2D import *
+from torch.utils.tensorboard import SummaryWriter
+import cProfile
+
+writer = SummaryWriter()
+
+############################################################## POINTS PLOTTING #############################################################
+############################################################################################################################################
+
+
+def plot_training_points(t_0, t_b, t_r, x_0, x_b, x_r, y_0, y_b, y_r, u_0, u_b):
+    """
+    Input: -dimension = spatial dimension
+           -time_x = [t_0,t_b,t_r,x_0,x_b,x_r,u_0,u_b]
+    Output: display training points in either 1,2 or 3D
+    """
+    fig = plt.figure(figsize=(9, 6))
+    ax = fig.add_subplot(projection='3d')
+    ax.scatter(t_0, x_0[:, 0], y_0[:, 0], c=u_0, marker='X', vmin=-1, vmax=1)
+    ax.scatter(t_b, x_b[:, 0], y_b[:, 0], c=u_b, marker='X', vmin=-1, vmax=1)
+    ax.scatter(t_r, x_r[:, 0], y_r[:, 0], c='r', marker='.', alpha=0.1)
+    ax.set_xlabel('$t$')
+    ax.set_ylabel('$x1$')
+    ax.set_zlabel('$y1$')
+    ax.set_title('Positions of collocation points and boundary data')
+    plt.show()
+
+########################################################## PLOTTING FUNCTIONS ###########################################################
+##########################################################################################################################################
+
+
+def plot1dgrid_real(lb, ub, N, model, k, with_rnn=False):
+    """Same for the real solution"""
+    model = model.net
+    x1space = np.linspace(lb[0], ub[0], N)
+    y1space = np.linspace(lb[0], ub[0], N)
+    tspace = np.linspace(lb[1], ub[1], N)
+    T, X1, Y1 = np.meshgrid(tspace, x1space, y1space)
+    T = torch.from_numpy(T).view(1, N**3, 1).to(device).float()
+    X1 = torch.from_numpy(X1).view(1, N**3, 1).to(device).float()
+    Y1 = torch.from_numpy(Y1).view(1, N**3, 1).to(device).float()
+    if not with_rnn:
+        T = T.transpose(0, 1).squeeze(-1)
+        X1 = X1.transpose(0, 1).squeeze(-1)
+        Y1 = Y1.transpose(0, 1).squeeze(-1)
+    else:
+        T = T.transpose(0, 1)
+        X1 = X1.transpose(0, 1)
+        Y1 = Y1.transpose(0, 1)
+    upred = model(torch.cat((X1, Y1, T), 1))
+    U = torch.squeeze(upred).detach().cpu().numpy()
+    U = upred.view(N, N, N).detach().cpu().numpy()
+    T, X1 = T.view(N, N, N).detach().cpu().numpy(), X1.view(
+        N, N, N).detach().cpu().numpy()
+    Y1 = Y1.view(N, N, N).detach().cpu().numpy()
+    z_array = np.zeros((N, N, N))
+    for i in range(N):
+        z_array[:, :, i] = U[i]
+
+    fig = plt.figure()
+    ax = fig.add_subplot(projection='3d')
+    ax.scatter(T, X1, Y1, c=U, marker='X')
+    ax.set_xlabel('$t$')
+    ax.set_ylabel('$x1$')
+
+    plt.savefig(f'results/real_sol_{k}')
+    writer.add_figure(f'real_sol_{k}', fig)
+    plt.close()
+
+# Plot train and val losses on same figure
+
+
+def plot_loss(train_losses, val_losses, accuracy):
+    fig, (ax1, ax2) = plt.subplots(2, 1)
+    plt.style.use('dark_background')
+    ax1.plot(train_losses, label='train')
+    ax1.plot(val_losses, label='val')
+    ax2.plot(accuracy, label="accuracy", color='red')
+
+    ax1.set(ylabel='Loss')
+    ax2.set(ylabel='Error')
+    plt.xlabel('Epoch')
+
+    ax1.legend()
+    ax2.legend()
+    plt.savefig(f'results/loss')
+    plt.close()
+
+########################################################### TRAINING ###########################################################
+################################################################################################################################
+
+
+def train(model, train_data, val_data, train_data_begin,
+          epochs):
+    epochs = tqdm(range(epochs), desc="Training")
+    losses = []
+    val_losses = []
+    acc = []
+    for epoch in epochs:
+        # Shuffle train_data
+        index_shuf_r = torch.randperm(train_data[0].shape[0])
+        index_shuf_b = torch.randperm(train_data[3].shape[0])
+        index_shuf_i = torch.randperm(train_data[5].shape[0])
+        x_r_train = train_data[0][index_shuf_r].to(device)
+        y_r_train = train_data[1][index_shuf_r].to(device)
+        t_r_train = train_data[2][index_shuf_r].to(device)
+        u_b_train = train_data[3][index_shuf_b].to(device)
+        x_b_train = train_data[4][index_shuf_b].to(device)
+        y_b_train = train_data[5][index_shuf_b].to(device)
+        t_b_train = train_data[6][index_shuf_b].to(device)
+        u_i_train = train_data[7][index_shuf_i].to(device)
+        x_i_train = train_data[8][index_shuf_i].to(device)
+        y_i_train = train_data[9][index_shuf_i].to(device)
+        t_i_train = train_data[10][index_shuf_i].to(device)
+        train_data_new = [x_r_train, y_r_train, t_r_train, u_b_train,
+                          x_b_train, y_b_train, t_b_train, u_i_train, x_i_train, y_i_train, t_i_train]
+        # Shuffle train_data_begin
+        index_shuf_b = torch.randperm(train_data_begin[0].shape[0])
+        x_b_train = train_data_begin[1][index_shuf_b].to(device)
+        y_b_train = train_data_begin[2][index_shuf_b].to(device)
+        t_b_train = train_data_begin[0][index_shuf_b].to(device)
+
+        train_data_begin = [t_b_train, x_b_train, y_b_train]
+        train_data = train_data_new
+        if epoch < 1000:
+            loss_begin = model.train_step(train_data_begin, phase="beginning")
+            epochs.set_postfix(loss=loss_begin)
+        else:
+            loss_residual, loss_bords, loss_init, loss_bords_der, loss_trunc = model.train_step(
+                train_data)
+            loss = loss_residual + loss_bords + loss_init + loss_bords_der + loss_trunc
+            val_loss = model.val_step(val_data)
+            accuracy = model.accuracy_step(val_data)
+            epochs.set_postfix(loss_residual=loss_residual,
+                               loss_bords=loss_bords,
+                               loss_init=loss_init,
+                               loss_bords_der=loss_bords_der,
+                               loss_trunc=loss_trunc,
+                               loss=loss,
+                               val_loss=val_loss,
+                               accuracy=accuracy)
+
+            # Scheduler step
+            model.scheduler.step(val_loss)
+
+            # Append loss lists (and eventually log for Tensorboard)
+            losses.append(loss)
+            val_losses.append(val_loss)
+            acc.append(accuracy)
+
+            writer.add_scalar('Loss_residual', loss_residual, epoch)
+            writer.add_scalar('Loss_bords', loss_bords, epoch)
+            writer.add_scalar('Loss_init', loss_init, epoch)
+            writer.add_scalar('Loss_bords_der', loss_bords_der, epoch)
+            writer.add_scalar('Loss_trunc', loss_trunc, epoch)
+            writer.add_scalar('Loss', loss, epoch)
+            writer.add_scalar('Val_loss', val_loss, epoch)
+            writer.add_scalar('Accuracy', accuracy, epoch)
+
+            plot_loss(losses, val_losses, acc)
+
+        if epoch % 100 == 0:
+            plot1dgrid_real(lb, ub, N_plotting, model, epoch)
+
+        if epoch % 1000 == 0:
+            torch.save(model.net.state_dict(), f"results/model_{epoch}.pt")
+
+
+if __name__ == '__main__':
+    seed = 42
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = True
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(device)
+
+    N_i, N_b, N_r = DEFAULT_CONFIG['N_i'], DEFAULT_CONFIG['N_b'], DEFAULT_CONFIG['N_r']
+    l_b, u_b = DEFAULT_CONFIG['l_b'], DEFAULT_CONFIG['u_b']
+    N_neurons, N_layers = DEFAULT_CONFIG['N_neurons'], DEFAULT_CONFIG['N_layers']
+
+    with_rnn = False
+    net = PINN(with_rnn=with_rnn, N_neurons=N_neurons, N_layers=N_layers)
+    # net._model_summary()
+
+    # Write config to tensorboard
+    writer.add_text('Config', str(DEFAULT_CONFIG))
+
+    t_ri, x_ri, y_ri = define_points_begin(10000, l_b, u_b)
+    t_i, x_i, y_i, u_i, t_b, x_b, y_b, u_b, t_r, x_r, y_r = define_points(
+        N_i, N_b, N_r, l_b, u_b)
+    # x_r,t_r,u_b,x_b,t_b,u_i,x_i,t_i = normalize_data(x_r,t_r,
+    #    u_b,x_b,t_b,
+    #    u_i,x_i,t_i)
+    plot_training_points(t_i.data.numpy(),
+                         t_b.data.numpy(),
+                         t_r.data.numpy(),
+                         x_i.data.numpy(),
+                         x_b.data.numpy(),
+                         x_r.data.numpy(),
+                         y_i.data.numpy(),
+                         y_b.data.numpy(),
+                         y_r.data.numpy(),
+                         u_i.data.numpy(),
+                         u_b.data.numpy())
+
+    train_data, val_data = val_split(
+        x_r, y_r, t_r, u_b, x_b, y_b, t_b, u_i, x_i, y_i, t_i, split=0.1)
+
+    train_data_begin = [t_ri.to(device), x_ri.to(device), y_ri.to(device)]
+    lb = [0, 0]
+    ub = [1, 1]
+    N_plotting = DEFAULT_CONFIG['N_plotting']
+    epochs = DEFAULT_CONFIG['epochs']
+
+    train(net, train_data, val_data, train_data_begin, epochs=epochs)
+
+    writer.flush()
+    writer.close()
+
+    net.net.load_state_dict(torch.load("model_9000.pt"))
+    plot1dgrid_real(lb, ub, N_plotting, net, 10000, show=True)
